c018ee8fecd4e28230df0a3b4b26c806490828ed
monorural <57811172+monorural@users.noreply.github.com>
2023-03-24 19:54:42 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -14,6 +14,8 @@
                 const [ini, fin, step] = range_x.split(",").map(Number);
 
     let y = "";
+                let step_text = document.getElementById("inputNumber");
+                let step = parseFloat(step_text.value);
     if (step !== 0) {
         for (let x = ini; x < fin; x += step) {
             const foo = eval("`" + translation_x + "`");
