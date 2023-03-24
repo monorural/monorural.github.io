2ef2c4b7fad09873beb81fb7b5ace0450620715b
monorural <57811172+monorural@users.noreply.github.com>
2023-03-24 20:03:44 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,21 +1,9 @@
-        function multiplyByTwo() {
-            const inputNumber = document.getElementById("inputNumber");
-            const resultElement = document.getElementById("result");
-            const number = parseFloat(inputNumber.value);
-
-        /*    if (!isNaN(number)) {
-                const result = number * 2;
-                resultElement.textContent = result;
-            } else {
-                resultElement.textContent = "Invalid input";
-         */  if (true) {
-                const translation_x = "{x}:(0), {x+6}:(1), {x+12}:(0),";
-                const range_x = "0,36,18";
-                const [ini, fin, step] = range_x.split(",").map(Number);
+if (true) {
+    const translation_x = "{x}:(0), {x+6}:(1), {x+12}:(0),";
+    const range_x = "0,36,18";
+    const [ini, fin, step] = range_x.split(",").map(Number);
 
     let y = "";
-                let step_text = document.getElementById("inputNumber");
-                let step = parseFloat(step_text.value);
     if (step !== 0) {
         for (let x = ini; x < fin; x += step) {
             const foo = eval("`" + translation_x + "`");
@@ -24,7 +12,5 @@
         y = y.trim().replace(/,$/, "");
     }
 
-    resultElement.textContent = y;
+    console.log(y);
 }
-            }
-        }
