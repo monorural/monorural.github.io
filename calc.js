f4fba194b0586114c436cd279f4b5b8b5501a7ff
monorural <57811172+monorural@users.noreply.github.com>
2023-03-24 19:50:42 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -3,10 +3,26 @@
             const resultElement = document.getElementById("result");
             const number = parseFloat(inputNumber.value);
 
-            if (!isNaN(number)) {
+        /*    if (!isNaN(number)) {
                 const result = number * 2;
                 resultElement.textContent = result;
             } else {
                 resultElement.textContent = "Invalid input";
+         */  if (true) {
+                const translation_x = "{x}:(0), {x+6}:(1), {x+12}:(0),";
+                const range_x = "0,36,18";
+                const [ini, fin, step] = range_x.split(",").map(Number);
+
+    let y = "";
+    if (step !== 0) {
+        for (let x = ini; x < fin; x += step) {
+            const foo = eval("`" + translation_x + "`");
+            y += foo;
+        }
+        y = y.trim().replace(/,$/, "");
+    }
+
+    resultElement.textContent = y;
+}
             }
         }
